a54be740e0c423a947eb5ff122a04d1db76bc56b
Aman Lajpal <42869088+amanlajpal@users.noreply.github.com>
2025-02-23 19:49:25 +0530

    Add metadata for dashboard, customers, invoices, and login pages

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,12 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Edit Invoice",
+};
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -10,7 +15,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
-  ])
+  ]);
 
   if (!invoice) {
     notFound();
