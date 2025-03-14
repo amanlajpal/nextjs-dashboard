91e5594a8594f3ba1a61163db6ade337b64ae548
Aman Lajpal <42869088+amanlajpal@users.noreply.github.com>
2025-03-14 13:09:40 +0530

    [feat]: [#2] Update customer page to support search parameters and pagination

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -6,7 +6,15 @@ export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default async function Page() {
-  const customers = await fetchFilteredCustomers("");
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = searchParams?.page || 1;
+  const customers = await fetchFilteredCustomers(query);
   return <CustomersTable customers={customers} />;
 }
